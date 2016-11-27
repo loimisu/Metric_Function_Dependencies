dc1e358716801ea60492bccd0413cc3a10744043
Yanwen.Zhou <yanwen.zhou@outlook.com>
2016-11-26 23:29:39 -0500

    initial algorithms created, and update parse code

---
+++ mf_values_init.py
@@ -0,0 +1,41 @@
+import numpy as np
+import scipy as sp
+import scipy.stats
+from kmeansd1 import ckmeans
+
+# X = [190,175,175,175,175,175,170,170,197,197,197,190,
+# 	 190,179,179,179,179,179,173,190,190,170,197,190,
+# 	 172,152,190,172,152,190,197,197,190,197,197,190,
+# 	 197,197,190,197,172,152,190]
+
+def mf_durations(dataset):
+	# confidence interval of Gaussian Distribution
+	def GCI_values(dataset,confidence=0.95):
+		n = len(dataset)
+		m, se = np.mean(dataset), scipy.stats.sem(dataset)
+		h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+		GCI_MIN = int(m-h)+1 
+		GCI_MAX = int(m+h) # use integer space instead fload
+		GCI_values = []
+		for item in dataset:
+			if item>=GCI_MIN and item<=GCI_MAX and item not in GCI_values:
+			        GCI_values.append(item)
+		return GCI_values
+
+	# This function is to get the most frequent values which clustering by ckmeans
+	def ckmeans_mf_values(dataset): 
+		mf_values = [] #most frequent values
+		dataset = map(lambda item: float(item)/100, dataset)
+		clusters = ckmeans(dataset, 3)
+		mf_set= max(clusters, key=len) # most frequent set
+		for x in mf_set:
+		    if x not in mf_values:
+		        mf_values.append(x) #get the distinct values from mf_set
+		mf_values = map(lambda item: int(item*100), mf_values)
+		return mf_values
+
+	return list(set(GCI_values(dataset)) | set(ckmeans_mf_values(dataset)))
+
+# print mf_durations(X)
+
+
--- parse_to_df.py
+++ parse_to_df.py
@@ -5,12 +5,12 @@ import pandas as pd
 import os
 
 if __name__ == '__main__':
-    #filepath="/home/hduser/git/Metric_Function_Dependencies/clean_flight/2011-12-01-data.txt"
-    path = "/home/hduser/git/Metric_Function_Dependencies/clean_flight"
-for file in os.listdir(path):
-    if file.endswith(".txt"):
-        filepath = path + "/" +file
-        infile = open(filepath,"r")
+    filepath="/home/hduser/git/Metric_Function_Dependencies/clean_flight/2011-12-11-data.txt"
+#     path = "/home/hduser/git/Metric_Function_Dependencies/clean_flight"
+# for file in os.listdir(path):
+#     if file.endswith(".txt"):
+#         filepath = path + "/" +file
+    infile = open(filepath,"r")
     
     def filter_input(data):
         return len(data.strip()) # removes '\n' from the beginning and end of the string
@@ -23,7 +23,8 @@ for file in os.listdir(path):
     time3 = []
     time4 = []
 
-    patRemove = re.compile(r'[ -]*(Contact|Not|Delay|On Time|&nbsp|HST|AKST|PST|MST|CST|EST).*|[ ]*\(.*\).*$|^.*Term.*$|[,]|( - )|[*]', re.IGNORECASE)
+
+    patRemove = re.compile(r'[ -]*(Contact|Not|Delay|On Time|&nbsp|HST|AKST|PST|MST|CST|EST|Sun|Sat|Mon|Tues|Tue|Wed|Thurs|Thur|Thu|Fri).*|[ ]*\(.*\).*$|^.*(Term|B32).*$|.*[,*.].*|.*( - ).*', re.IGNORECASE)
     # remove string with ' - contact%' or 'not%' on item
     # remove string with 'delay%' or 'on time%' on item
     # remove stiing with with '&nbsp'
@@ -37,28 +38,28 @@ for file in os.listdir(path):
     for aline in infile:
         data = filter(filter_input, aline.split('\t')) 
         # remove '\t' from the beginning and end of the string
-        #if data[0] in ("aa", "ua"):
-        data = map(lambda item: patRemove.sub(' ', item), data) 
-        
-        datalist = filter(lambda item: len(item) > 8, data[1:])
-        datalist.insert(0,data[0])    
-        #datalist = map(lambda item: re.sub(r'([1-9]+[ap][a-z]*)',' ', item, re.IGNORECASE), datalist)
-        #replace a/p to am/pm
+        if len(data[0])>=3 and data[1] in ("UA-1534-RTB-IAH", "AA-2098-ORD-BOS"):
+            data = map(lambda item: patRemove.sub(' ', item), data) 
+            
+            #datalist = filter(lambda item: len(item) > 8, data[1:])
+            datalist = map(lambda item: re.findall(r'[0-9]+:[0-9]+[ ]*[APap]*[Mm]*', item), data[2:])
 
-        n = 6 - len(datalist)
-        if len(datalist) <= 6:
-            datalist.extend(['null']*n)
+            datalist.insert(0,data[1])    
+            
+            #replace a/p to am/pm
 
-        source_nm.append(datalist[0])
-        flight_nm.append(datalist[1])
-        time1.append(datalist[2])
-        time2.append(datalist[3])
-        time3.append(datalist[4])
-        time4.append(datalist[5])
-        # if item index not exist, then set as 'null'   
+            n = 5 - len(datalist)
+            if len(datalist) <= 5:
+                datalist.extend(['NaN']*n)
+
+            flight_nm.append(datalist[0])
+            time1.append(datalist[1])
+            time2.append(datalist[2])
+            time3.append(datalist[3])
+            time4.append(datalist[4])
+            # if item index not exist, then set as 'null'   
       
     data = {
-        'source_nm': source_nm,
         'flight_nm': flight_nm,
         'time1': time1,
         'time2': time2,
@@ -67,13 +68,6 @@ for file in os.listdir(path):
     }
     
     df = pd.DataFrame(data)
-    #print df
-    df.to_csv('/home/hduser/git/Metric_Function_Dependencies/output01.csv')
-    print file + " is successully exported to csv!"
-    
-            
-
-            
-            
-                    
-            
+    print df
+#     df.to_csv('/home/hduser/git/Metric_Function_Dependencies/output01.csv')
+# print file + " is successully exported to csv!"
